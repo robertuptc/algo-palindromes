559107a1694ca30c179f3ae97ef27d38b6796f7f
robertuptc <robertuptc@gmail.com>
2022-05-30 11:53:14 -0400

    finished exercise

--- js/palindrome.js
+++ js/palindrome.js
@@ -1,3 +1,10 @@
 exports.palindrome = function(word) {
+    originalWord = word.toString().toLowerCase().replace(/[^a-zA-z0-9]/g, "").split('').join('')
+    reversedWord = word.toString().split('').reverse().join('').toLowerCase().replace(/[^a-zA-z0-9]/g, "")
 
+    return originalWord == reversedWord ? true : false
 };
+
+
+
+
--- python/palindrome.py
+++ python/palindrome.py
@@ -1,2 +1,11 @@
+import re
 def palindrome(word):
-    # Write code here
+    word_to_string = str(word)
+    original_word = re.sub(r'[^a-zA-Z0-9]', "", word_to_string.lower())
+    reversed_word = re.sub(r'[^a-zA-Z0-9]', "", word_to_string[::-1].lower())
+
+    return original_word == reversed_word
+
+
+
+
